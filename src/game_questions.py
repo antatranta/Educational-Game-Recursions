55bdbc9541a067d94b26c820ea6579d8bb4ab4cc
Anthony Tran <a_tran38@u.pacific.edu>
2019-02-22 11:38:42 -0800

    Refactor GameQuestions class so it uses a list instead of a switch TODO: still need to compare answers passing that on to Seth
    Signed-off-by: Anthony Tran <a_tran38@u.pacific.edu>

--- src/game_questions.py
+++ src/game_questions.py
@@ -10,19 +10,33 @@ class GameQuestions:
     def __init__(self, seed):
         random.seed(seed)
         self.question_number = 0
+        self.questions = []
+        self._add_questions_()
+
+    def _add_questions_(self):
+        """ Private method that adds questions to the list defined in init """
+        question_0 = "When you start the game, what recursion level do you start on?"
+        self.questions.append(question_0)
+
+        question_1 = "What\'s the max level recursion can you achieve with this game?"
+        self.questions.append(question_1)
+
+        question_2 = "What recursion level are your parents in?"
+        self.questions.append(question_2)
+
+        question_3 = "What recursion level is your grandmother in?"
+        self.questions.append(question_3)
+
+        question_4 = "If there wasn\'t a max level in this tree, what level would your " \
+                     "great great grandparents be located in?"
+        self.questions.append(question_4)
 
     def game_questions(self):
-        """ The list of game questions to pick at randomly """
-        self.question_number = random.randint(0, 4)
+        """ The list of game questions to pick at randomly returns a string of the
+            question """
+        self.question_number = random.randint(0, len(self.questions) - 1)
 
-        return {
-            0: "When you start the game, what recursion level do you start on?",
-            1: "What\'s the max level recursion can you achieve with this game?",
-            2: "What recursion level are your parents in?",
-            3: "What recursion level is the grandmother in?",
-            4: "If there wasn't a max level in this tree, what level would the great great"
-               "grandparents be located in?",
-        }[self.question_number]
+        return self.questions.pop(self.question_number)
 
     def compare_answers(self, player_input):
         """ Compares the answers with the user input corresponding to the question """
